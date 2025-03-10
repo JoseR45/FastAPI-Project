44c1ec5f2731ad641f2bf7ec3bfb21eb7e5f91f6
José Ramón <jose.1999.rfg@gmail.com>
2025-03-10 11:54:14 -0400

    feat(main):  init fastapi, import middlewares and models

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from settings.settings import MIDDLEWARES, ROUTES
+
+app = FastAPI()
+
+for middleware, options in MIDDLEWARES:
+        app.add_middleware(middleware, **options)
+
+for router, prefix in ROUTES:
+        app.include_router(router, prefix=prefix)
+
+@app.get("/")
+def read_root():
+    return {"message": "Prueba"}
+
+    
