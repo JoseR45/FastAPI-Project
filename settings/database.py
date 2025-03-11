1fba800da73512e6cd368b9373199260439040b0
José Ramón <jose.1999.rfg@gmail.com>
2025-03-11 19:47:17 -0400

    feat(database): update to async session

--- settings/database.py
+++ settings/database.py
@@ -1,16 +1,24 @@
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
+DATABASE_URL = "postgresql+asyncpg://postgres:root@localhost/fastapi"
 
-DATABASE_URL = "postgresql://postgres:root@localhost/fastapi"
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+async_engine = create_async_engine(DATABASE_URL, echo=True)
+
+AsyncSessionLocal = sessionmaker(
+    bind=async_engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session  
+        
+        
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
